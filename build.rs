1916ba35ec117a7c320d5f498df6af1cd816208a
Jason Ozias <jason.ozias@gmail.com>
2018-10-02 23:15:03 -0400

    updated to newest vergen

--- build.rs
+++ build.rs
@@ -1,21 +1,11 @@
 extern crate vergen;
 
-use vergen::{ConstantsFlags, Result, Vergen};
+use vergen::{generate_cargo_keys, ConstantsFlags};
 
 fn main() {
-    gen_constants().expect("Unable to generate vergen constants!");
-}
-
-fn gen_constants() -> Result<()> {
     let mut flags = ConstantsFlags::all();
     flags.toggle(ConstantsFlags::SEMVER_LIGHTWEIGHT);
     flags.toggle(ConstantsFlags::SHA_SHORT);
     flags.toggle(ConstantsFlags::BUILD_DATE);
-    let vergen = Vergen::new(flags)?;
-
-    for (k, v) in vergen.build_info() {
-        println!("cargo:rustc-env={}={}", k.name(), v);
-    }
-
-    Ok(())
+    generate_cargo_keys(flags).expect("Unable to generate the cargo keys!");
 }
