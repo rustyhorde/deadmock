cb8bcaf9a8fff9d32e4a74195209c83f5c143370
Jason Ozias <jason.ozias@kroger.com>
2018-10-12 09:00:02 -0400

    gitignore, locustfile work

--- scripts/locustfile.py
+++ scripts/locustfile.py
@@ -3,49 +3,59 @@ from locust import HttpLocust, TaskSet, task
 class UserBehavior(TaskSet):
     @task(1)
     def proxy_healthcheck(self):
-        self.client.get("/mobilecheckout/healthcheck", name = "Proxy - Healthcheck")
+        self.client.get("/mobilecheckout/healthcheck",
+            name = "Exact Match (Method, URL, Proxy, healthcheck)")
 
     @task(1)
     def proxy_get_card(self):
-        self.client.get("/mobilecheckout/api/v1/card/b14d93bd-67c2-aa1b-c973-f6b084403e3e", name = "Proxy - Get Card")
+        self.client.get("/mobilecheckout/api/v1/card/b14d93bd-67c2-aa1b-c973-f6b084403e3e",
+            name = "Exact Match (URL, Method, Proxy, card")
 
     @task(1)
     def proxy_jasonoziascom(self):
-        self.client.get("/", name = "Proxy - jasonozias.com")
+        self.client.get("/",
+            name = "Exact Match (URL, Method, Proxy, jasonozias.com)")
 
     @task(10)
     def get_card_visa_static(self):
         self.client.get("/mobilecheckout/api/v1/card",
             headers = { "X-Card-Type": "VS" },
-            name = "Static - Get Card (Visa)")
+            name = "Exact Match (URL, Method, Header, Visa)")
 
     @task(10)
     def get_card_mastercard_static(self):
         self.client.get("/mobilecheckout/api/v1/card",
             headers = { "X-Card-Type": "MC" },
-            name = "Static - Get Card (Mastercard)")
+            name = "Exact Match (URL, Method, Header, Mastercard)")
 
     @task(10)
     def plaintext_static(self):
-        self.client.get("/plaintext", name = "Static - Plaintext")
+        self.client.get("/plaintext",
+            name = "Exact Match (URL, plaintext)")
 
     @task(10)
     def json_static(self):
-        self.client.get("/json", name = "Static - JSON")
+        self.client.get("/json",
+            name = "Exact Match (URL, json)")
 
     @task(10)
     def json_static_not_found(self):
         with self.client.get("/json",
             headers = { "X-Correlation-Id": "not-found" },
-            name = "Static - JSON (404 Not Found)",
+            name = "Exact Match (URL, Header)",
             catch_response = True) as response:
             if response.status_code == 404:
                 response.success()
 
     @task(10)
     def weather_static(self):
-        self.client.get("/weather/45039", name = "Static - Weather")
+        self.client.get("/weather/45039", name = "Pattern Match (URL)")
 
+    @task(10)
+    def pattern_match_header(self):
+        self.client.get("/header-pattern",
+            headers = { "X-Pattern-Match": "yoda-bloda" },
+            name = "Pattern Match (Header)")
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
